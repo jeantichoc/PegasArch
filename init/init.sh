d20ba69c5fff702f77b131a0bd07b976fdaadbd3
jeantichoc <jean.bottein@gmail.com>
2022-06-09 01:14:38 +0200

    fix: pegasus as default frontend

--- init/init.sh
+++ init/init.sh
@@ -2,6 +2,7 @@ eval $(cat ${SCRIPTPATH:-.}/../config.txt  | sed -e '/#########/,$d' | grep -v "
 source "${SCRIPTPATH:-.}/../init/functions.sh"
 
 ##### ADDITONALS CONFIGURATIONS ####
+frontend=pegasus
 frontend_conf="$HOME/.var/app/org.pegasus_frontend.Pegasus/config/pegasus-frontend"
 scraper_cmd="$HOME/GitHub/skyscraper/Skyscraper"
 scraper_launcher="$SCRIPTPATH/launch.sh \"{file.path}\""
