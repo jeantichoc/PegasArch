05c24b44266cb0d0a34ac24ded68e5b6a835228d
jeantichoc <jean.bottein@gmail.com>
2022-06-13 15:26:25 +0200

    feat: centralize in PegaArch.sh

--- PegasArch.sh
+++ PegasArch.sh
@@ -4,11 +4,13 @@ SCRIPTPATH="$(dirname "$SCRIPT")"
 
 menu="$1"
 
+
 if [[ $menu == scrap ]] ; then
   shift
-  bash $SCRIPTPATH/scripts/scrap.sh $*
+  bash $SCRIPTPATH/scripts/scrap.sh "$@"
   exit $?
 fi
+
 
 if [[ $menu == refresh ]] ; then
   shift
@@ -18,7 +20,40 @@ if [[ $menu == refresh ]] ; then
   exit $?
 fi
 
+
 if [[ $menu == pegasus ]] ; then
+  shift
   flatpak run org.pegasus_frontend.Pegasus &
+  exit $?
+fi
+
+
+if [[ $menu == sync ]] ; then
+  shift
+  get_ids_to_sync | while read -r PLATFORM; do
+    rcloneSync "$ROMSDIR/$PLATFORM"
+  done
+
+  rclone_bisync "$SAVDIR"
+  exit $?
+fi
+
+
+if [[ $menu == mount ]] ; then
+  shift
+  #TODO remove empty dir
+  get_ids_to_mount | while read -r PLATFORM; do
+    rclone_mount "$ROMSDIR/$PLATFORM"
+  done
+  exit $?
+fi
+
+
+
+if [[ $menu == launch ]] ; then
+  shift
+  #TODO remove empty dir
+
+  bash $SCRIPTPATH/scripts/launch.sh "$@"
   exit $?
 fi
