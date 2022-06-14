8aa80138f1806b3ed8d4ca26fe19cc354d7c1ecc
jeantichoc <jean.bottein@gmail.com>
2022-06-14 15:32:38 +0200

    feat: better location default installation and version management

--- init/update.sh
+++ init/update.sh
@@ -11,16 +11,27 @@ function handle_error () {
   local exit_code=$?
   local action=$1
   rm --force version
-  echo.blue "--- Failed to $action $app v.${latest}, exiting with code $exit_code ---"
+  echo.blue "--- Failed to $action $app $new, exiting with code $exit_code ---"
   exit $exit_code
 }
 
-
+# parameter to force a specific version
 if [[ $1 ]] ; then
-  latest="$1"
+  new="$1"
 else
-  latest=$(wget -q -O - "https://api.github.com/repos/$org/$app/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+  new=$(wget -q -O - "https://api.github.com/repos/$org/$app/releases/new" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
 fi
+
+# go and/or create folder
+if [[ -f PegasArch.sh ]] ; then
+  pegasarch_path=.
+elif [[ -f ../PegasArch.sh ]] ; then
+  pegasarch_path=..
+else
+  read -p "Path for installation: " -i $HOME/GitHub/PegasArch -e pegasarch_path
+fi
+mkdir -p $pegasarch_path || exit 1
+cd $pegasarch_path || exit 2
 
 
 if [[ ! -f version ]] ; then
@@ -29,19 +40,19 @@ fi
 source version
 
 
-if [[ $latest == $version ]] ; then
-  echo.blue "--- $app is already the latest version, exiting ---"
-  echo.blue "You can force a reinstall by removing the version file by running rm version. Then rerun ./update_$app.sh afterwards."
+if [[ -z $1 && $new == $version ]] ; then
+  echo.blue "--- $app is already the version $version, exiting ---"
+  echo.blue "You can force a reinstall by removing the version file by running rm version. Then rerun update.sh afterwards."
   exit 0
 fi
 
 
-echo.blue "--- Fetching $app v.$latest ---"
-if [[ $1 == HEAD ]] ; then
-  url="https://github.com/$org/$app/archive/refs/heads/main.tar.gz"
-  latest=main
+echo.blue "--- Fetching $app $new ---"
+if [[ $1 ]] ; then
+  new=$1
+  url="https://github.com/$org/$app/archive/refs/heads/$new.tar.gz"
 else
-  url="https://github.com/$org/$app/archive/${latest}.tar.gz"
+  url="https://github.com/$org/$app/archive/$new.tar.gz"
 fi
 wget -N "$url" || handle_error "fetch"
 
@@ -52,8 +63,8 @@ cp -p config.txt .config.txt 2>/dev/null
 cp -p resources/artwork.xml resources/.artwork.xml 2>/dev/null
 cp -p resources/retroarch.conf resources/.retroarch.conf 2>/dev/null
 
-tar xvzf $latest.tar.gz --strip-components 1 --overwrite || handle_error "unpack"
-rm $latest.tar.gz
+tar xvzf $new.tar.gz --strip-components 1 --overwrite || handle_error "unpack"
+rm $new.tar.gz
 
 mv .config.txt config.txt 2>/dev/null
 mv resources/.artwork.xml resources/artwork.xml 2>/dev/null
@@ -62,11 +73,12 @@ mv resources/.retroarch.conf resources/retroarch.conf 2>/dev/null
 
 echo.blue "--- Cleaning out old build if one exists ---"
 
-echo.blue "--- Installing $app v.$latest ---"
+echo.blue "--- Installing $app $new ---"
 
 echo.blue "--- Installing dependencies ---"
 bash init/install_dependencies.sh || handle_error "install"
 
-echo.blue "--- $app has been updated to v.$latest ---"
+echo.blue "--- $app has been updated to $new ---"
+echo "version=$new" > version
 
 exit 0
