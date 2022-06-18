48695caf09fa134428db56e4511599b039c55e43
jeantichoc <jean.bottein@gmail.com>
2022-06-18 19:36:50 -0400

    fix: empty metadatas detection

--- src/pegasarch.bash
+++ src/pegasarch.bash
@@ -375,7 +375,7 @@ function install_libretro_cores () {
 
 function dir_empty_or_absent() {
   if [[ -d $1 ]] ; then
-    local lsA="$(ls -A "$1")" || return 1
+    local lsA="$(ls -A "$1/*/metadata.pegasus.txt")" || return 1
     if [[ $lsA ]] ; then
       echo false
       return
